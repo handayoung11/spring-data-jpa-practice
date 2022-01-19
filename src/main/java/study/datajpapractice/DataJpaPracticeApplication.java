048219c4fb951612a624b230fe928dc1a82a3698
JinYeongKim <handayoung11@gmail.com>
2022-01-19 23:18:35 +0900

    DataJpaPracticeApplication class 생성

---
+++ src/main/java/study/datajpapractice/DataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package study.datajpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaPracticeApplication.class, args);
+	}
+
+}
