6a79400a6484d68f8f322758da468c49f192856c
JinYeongKim <handayoung11@gmail.com>
2022-01-21 23:10:03 +0900

    Team Entity 수정

---
+++ src/main/java/study/datajpapractice/entity/Team.java
@@ -0,0 +1,27 @@
+package study.datajpapractice.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Setter(AccessLevel.NONE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
