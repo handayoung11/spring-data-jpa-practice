4471b10c73f867574cffa1477e0e43d75ddc8753
JinYeongKim <handayoung11@gmail.com>
2022-01-23 21:19:02 +0900

    MemberRepositoryTest.java에서 변수명 memberJpaRepository => memberRepository로 변경

--- src/test/java/study/datajpapractice/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpapractice/repository/MemberRepositoryTest.java
@@ -16,14 +16,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberJpaRepository;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA", 20, null);
-        Member savedMember = memberJpaRepository.save(member);
+        Member savedMember = memberRepository.save(member);
 
-        Member findMember = memberJpaRepository.findById(savedMember.getId()).orElse(null);
+        Member findMember = memberRepository.findById(savedMember.getId()).orElse(null);
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
@@ -35,21 +35,21 @@ class MemberRepositoryTest {
         Member member1 = new Member("member1", 20, null);
         Member member2 = new Member("member2", 20, null);
 
-        memberJpaRepository.save(member1);
-        memberJpaRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
         // 단건조회 검증
-        Member findMember1 = memberJpaRepository.findById(member1.getId()).orElse(null);
-        Member findMember2 = memberJpaRepository.findById(member2.getId()).orElse(null);
+        Member findMember1 = memberRepository.findById(member1.getId()).orElse(null);
+        Member findMember2 = memberRepository.findById(member2.getId()).orElse(null);
         assertThat(member1).isEqualTo(findMember1);
         assertThat(member2).isEqualTo(findMember2);
 
         // 리스트조회 및 카운트 검증
-        List<Member> members = memberJpaRepository.findAll();
-        assertThat(members.size()).isEqualTo(memberJpaRepository.count());
+        List<Member> members = memberRepository.findAll();
+        assertThat(members.size()).isEqualTo(memberRepository.count());
 
         // 삭제 검증
-        memberJpaRepository.delete(member1);
-        memberJpaRepository.delete(member2);
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
     }
 }
