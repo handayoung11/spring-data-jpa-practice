ae9b195ab5e0a3c6519ee470a8bafac7a33e4476
JinYeongKim <handayoung11@gmail.com>
2022-01-26 23:09:00 +0900

    @Query 어노테이션을 이용해 username과 age로 Member를 조회하는 JPQL을 메서드에 직접 매핑

--- src/main/java/study/datajpapractice/repository/MemberRepository.java
+++ src/main/java/study/datajpapractice/repository/MemberRepository.java
@@ -8,7 +8,11 @@ import study.datajpapractice.entity.Member;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findByNameAndAge(@Param("username") String username, @Param("age") int age);
 }
