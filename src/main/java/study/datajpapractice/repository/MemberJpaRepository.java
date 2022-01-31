644ba93973e685eae316a99eb6104e1b89be04ab
JinYeongKim <handayoung11@gmail.com>
2022-01-31 17:37:24 +0900

    나이로 페이징하는 pageByAge method 선언

--- src/main/java/study/datajpapractice/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpapractice/repository/MemberJpaRepository.java
@@ -53,4 +53,12 @@ public class MemberJpaRepository {
                 .setParameter("username", username)
                 .getResultList();
     }
+
+    public List<Member> pageByAge(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
