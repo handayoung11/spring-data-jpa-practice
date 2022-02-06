4b662f79a3d95847c72605169624d036f8e571af
JinYeongKim <handayoung11@gmail.com>
2022-02-06 18:18:59 +0900

    Spring Data JPA를 이용한 Auditing을 위한 설정 및 기본 클래스 생성

--- src/main/java/study/datajpapractice/DataJpaPracticeApplication.java
+++ src/main/java/study/datajpapractice/DataJpaPracticeApplication.java
@@ -2,7 +2,14 @@ package study.datajpapractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaPracticeApplication {
 
@@ -10,4 +17,15 @@ public class DataJpaPracticeApplication {
 		SpringApplication.run(DataJpaPracticeApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorAware() {
+//		return new AuditorAware<String>() {
+//
+//			@Override
+//			public Optional<String> getCurrentAuditor() {
+//				return Optional.of(UUID.randomUUID().toString());
+//			}
+//		};
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
