892da836e36695c6dfcfa3ffac75e4b6c2c7500b
JinYeongKim <handayoung11@gmail.com>
2022-02-07 01:11:04 +0900

    id를 입력받아 username을 반환하는 api 작성(findMember2: domain class converter 이용)

---
+++ src/main/java/study/datajpapractice/controller/MemberController.java
@@ -0,0 +1,33 @@
+package study.datajpapractice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpapractice.entity.Member;
+import study.datajpapractice.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new NullPointerException("존재하지 않는 회원입니다."));
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("memberA", 10, null));
+    }
+}
